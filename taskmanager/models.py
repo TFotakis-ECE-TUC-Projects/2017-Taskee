f9ad652bfaaf4f6c73b9105642ff2fffabd8a478
TFotakis <fotakistzanis@gmail.com>
2017-07-24 14:44:31 +0300

    Minor Taskmanager models changes

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -5,16 +5,22 @@ class TaskType(models.Model):
     name = models.CharField(max_length=250)
     weight = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     name = models.CharField(max_length=250)
-    type = TaskType()
+    type = models.ForeignKey(TaskType, on_delete=models.CASCADE, default=1)
     place = models.CharField(max_length=250)
     notes = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.name
+
 
 class WeeklySchedule(models.Model):
-    task = Task()
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, default=1)
     instanceId = models.IntegerField(default=0)
     day = models.CharField(max_length=9)
     startingTime = models.TimeField()
@@ -24,7 +30,7 @@ class WeeklySchedule(models.Model):
 
 
 class Availability(models.Model):
-    task = Task()
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, default=1)
     instanceId = models.IntegerField()
     day = models.CharField(max_length=9)
     startingTime = models.TimeField()
