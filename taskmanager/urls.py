d1ab845960edf587f1d61aa491995b2ddcbb4b9f
Manoz <emmanuel.gialitakis@gmail.com>
2017-07-25 14:32:51 +0300

    templates/taskmanager 2 new files (taskView,taskDetail)

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -6,4 +6,5 @@ app_name = 'taskmanager'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    #url(r'^task/(?P<pk>[0-9]+)/$', views.taskdetail, name='taskDetail'),
 ]
--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
-
-
-class IndexView(LoginRequiredMixin, generic.TemplateView):
-    template_name = 'taskmanager/index.html'
+from .models import Task
+from django.http import HttpResponse
+                                   #generic.TemplateView
+class IndexView(LoginRequiredMixin, generic.ListView):
+    template_name = 'taskmanager/taskView.html'
+    context_object_name = 'task_list'
     login_url = '/login/'
+
+    def get_queryset(self):
+        return Task.objects.all()
+
+def taskdetail(request, pk):
+    return HttpResponse("<h2>Details: "+str(pk))+"</h2>"
