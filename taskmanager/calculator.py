2ecb683e720394eb59e6089af1104d0cab5ee90b
TFotakis <fotakistzanis@gmail.com>
2017-08-02 15:47:34 +0300

    errorView Created and implemented when forms have errors - hasConflict implemented in weeklySchedule creation - general code optimization

--- taskmanager/calculator.py
+++ taskmanager/calculator.py
@@ -1,41 +1,30 @@
 from taskmanager.models import Day
 
 
-def Conflict_check(critical_startime,critical_endingtime,critical_day,critical_end_day,ws_endingTime,ws_startingTime,ws_day):
-    transicion1=False
-    if critical_endingtime < critical_startime:  # exoume transicion hmeras (Monday -> Tuesday)
-        critical_end_day = Day.objects.get(id=critical_day + 1)
-        transicion1 = True
-    else:
-        critical_end_day = critical_day
-    if ws_endingTime < ws_startingTime:  # exoume transicion hmeras (Monday -> Tuesday)
-        ws_end_day = Day.objects.get(id=ws_day.id + 1)
-        transicion2 = True
-    else:
-        transicion2 = False
-        ws_end_day = ws_day
-    sameDay = ws_day == critical_day
-    almost_sameDay1 = critical_end_day == ws_day
-    almost_sameDay2 = ws_end_day == critical_day
+def hasConflict(critical, ws):
+	criticalIsTransitive = critical.endingTime < critical.startingTime
+	critical_end_day = Day.objects.get(id=(ws.day.id - (not criticalIsTransitive) % 7) + 1)
+	wsIsTransitive = ws.endingTime < ws.startingTime
+	ws_end_day = Day.objects.get(id=(ws.day.id - (not wsIsTransitive) % 7) + 1)
 
-    check_days = sameDay  | almost_sameDay1 | almost_sameDay2
-    #
-    if transicion1 & transicion2:
-        return True
+	sameDay = ws.day == critical.day
+	almost_sameDay1 = critical_end_day == ws.day
+	almost_sameDay2 = ws_end_day == critical.day
+	# Todo: den vgazei noima????????
+	check_days = sameDay | almost_sameDay1 | almost_sameDay2
+	if criticalIsTransitive & wsIsTransitive:
+		return True
 
-    if transicion1 & (
-            not ((critical_startime >= ws_endingTime) & (critical_endingtime <= ws_startingTime))):
-        secondHalfTaken = (critical_startime < ws_endingTime) | (
-            critical_endingtime > ws_startingTime)  # & (critical_endingtime <= ws_startingTime)
-    else:
-        secondHalfTaken = (critical_startime <= ws_startingTime) & (critical_endingtime > ws_startingTime) & (critical_endingtime <= ws_startingTime)  # ([)]
+	if criticalIsTransitive & (not ((critical.startingTime >= ws.endingTime) & (critical.endingTime <= ws.startingTime))):
+		secondHalfTaken = (critical.startingTime < ws.endingTime) | (critical.endingTime > ws.startingTime)  # & (critical.endingTime <= ws.startingTime)
+	else:
+		secondHalfTaken = (critical.startingTime <= ws.startingTime) & (critical.endingTime > ws.startingTime) & (critical.endingTime <= ws.startingTime)  # ([)]
 
-    if transicion2 & (not ((critical_startime >= ws_endingTime) & (critical_endingtime <= ws_startingTime))):
-        firstHalfTaken = (critical_startime < ws_endingTime) | (critical_endingtime > ws_startingTime)  # & (ws_endingTime <= critical_endingtime)
-    else:
-        firstHalfTaken = (ws_startingTime <= critical_startime) & (critical_startime < ws_endingTime) & (ws_endingTime <= critical_endingtime)  # [(])
+	if wsIsTransitive & (not ((critical.startingTime >= ws.endingTime) & (critical.endingTime <= ws.startingTime))):
+		firstHalfTaken = (critical.startingTime < ws.endingTime) | (critical.endingTime > ws.startingTime)  # & (ws.endingTime <= critical.endingTime)
+	else:
+		firstHalfTaken = (ws.startingTime <= critical.startingTime) & (critical.startingTime < ws.endingTime) & (ws.endingTime <= critical.endingTime)  # [(])
 
-    if check_days & (secondHalfTaken | firstHalfTaken):  # wholeTaken |  | partlyTaken
-        return True
-
-    return False
+	if check_days & (secondHalfTaken | firstHalfTaken):  # wholeTaken |  | partlyTaken
+		return True
+	return False
