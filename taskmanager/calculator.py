bfe2fcb9d0cb88ad33d8b132dfe8df35d9d7524b
ManozGial <emmanuel.gialitakis@gmail.com>
2017-08-02 16:21:45 +0300

    Confilct Check function  optimization

--- taskmanager/calculator.py
+++ taskmanager/calculator.py
@@ -2,29 +2,30 @@ from taskmanager.models import Day
 
 
 def hasConflict(critical, ws):
-	criticalIsTransitive = critical.endingTime < critical.startingTime
-	critical_end_day = Day.objects.get(id=(ws.day.id - (not criticalIsTransitive) % 7) + 1)
-	wsIsTransitive = ws.endingTime < ws.startingTime
-	ws_end_day = Day.objects.get(id=(ws.day.id - (not wsIsTransitive) % 7) + 1)
+    criticalIsTransitive = critical.endingTime < critical.startingTime
+    critical_end_day = Day.objects.get(id=(ws.day.id - (not criticalIsTransitive) % 7) + 1)
+    wsIsTransitive = ws.endingTime < ws.startingTime
+    ws_end_day = Day.objects.get(id=(ws.day.id - (not wsIsTransitive) % 7) + 1)
 
-	sameDay = ws.day == critical.day
-	almost_sameDay1 = critical_end_day == ws.day
-	almost_sameDay2 = ws_end_day == critical.day
-	# Todo: den vgazei noima????????
-	check_days = sameDay | almost_sameDay1 | almost_sameDay2
-	if criticalIsTransitive & wsIsTransitive:
-		return True
+    sameDay = ws.day == critical.day
+    almost_sameDay1 = critical_end_day == ws.day
+    almost_sameDay2 = ws_end_day == critical.day
 
-	if criticalIsTransitive & (not ((critical.startingTime >= ws.endingTime) & (critical.endingTime <= ws.startingTime))):
-		secondHalfTaken = (critical.startingTime < ws.endingTime) | (critical.endingTime > ws.startingTime)  # & (critical.endingTime <= ws.startingTime)
-	else:
-		secondHalfTaken = (critical.startingTime <= ws.startingTime) & (critical.endingTime > ws.startingTime) & (critical.endingTime <= ws.startingTime)  # ([)]
+    check_days = sameDay | almost_sameDay1 | almost_sameDay2
+    if check_days:
+        if criticalIsTransitive & wsIsTransitive:
+            return True
 
-	if wsIsTransitive & (not ((critical.startingTime >= ws.endingTime) & (critical.endingTime <= ws.startingTime))):
-		firstHalfTaken = (critical.startingTime < ws.endingTime) | (critical.endingTime > ws.startingTime)  # & (ws.endingTime <= critical.endingTime)
-	else:
-		firstHalfTaken = (ws.startingTime <= critical.startingTime) & (critical.startingTime < ws.endingTime) & (ws.endingTime <= critical.endingTime)  # [(])
+        if criticalIsTransitive & (not ((critical.startingTime >= ws.endingTime) & (critical.endingTime <= ws.startingTime))):
+            secondHalfTaken = (critical.startingTime < ws.endingTime) | (critical.endingTime > ws.startingTime)  # & (critical.endingTime <= ws.startingTime)
+        else:
+            secondHalfTaken = (critical.startingTime <= ws.startingTime) & (critical.endingTime > ws.startingTime) & (critical.endingTime <= ws.startingTime)  # ([)]
 
-	if check_days & (secondHalfTaken | firstHalfTaken):  # wholeTaken |  | partlyTaken
-		return True
-	return False
+        if wsIsTransitive & (not ((critical.startingTime >= ws.endingTime) & (critical.endingTime <= ws.startingTime))):
+            firstHalfTaken = (critical.startingTime < ws.endingTime) | (critical.endingTime > ws.startingTime)  # & (ws.endingTime <= critical.endingTime)
+        else:
+            firstHalfTaken = (ws.startingTime <= critical.startingTime) & (critical.startingTime < ws.endingTime) & (ws.endingTime <= critical.endingTime)  # [(])
+
+        if  secondHalfTaken | firstHalfTaken:  # wholeTaken |  | partlyTaken
+            return True
+    return False
